040056318ce5ef5ae2926638b50c5c3122f4049c
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-01-09 11:41:49 -0400

    retornando a imagem

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -11,6 +11,14 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, // id
         ref: 'User' // referencia ao usuario
     }
+}, {
+    toJSON: {
+        virtuals: true,
+    },
 });
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`
+})
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -17,7 +18,8 @@ mongoose.connect('mongodb+srv://user:user@nodejs-aula-5g6a6.mongodb.net/aula?ret
 //req.body = Acessar corpo da requisição  
 
 app.use(cors());
-app.use(express.json());   
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));   
 app.use(routes);
 
 //porta
